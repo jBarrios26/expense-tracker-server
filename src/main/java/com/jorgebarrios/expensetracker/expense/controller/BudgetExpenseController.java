777a864de76bacd077a3ccb034bc846db382177a
jBarrios26 <jorge.barrios@galileo.edu>
2023-05-15 21:50:26 -0600

    Agregar clase de controllador de expenses.

---
+++ src/main/java/com/jorgebarrios/expensetracker/expense/controller/BudgetExpenseController.java
@@ -0,0 +1,11 @@
+package com.jorgebarrios.expensetracker.expense.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/budget-expense")
+@RequiredArgsConstructor
+@RestController
+public class BudgetExpenseController {
+}
